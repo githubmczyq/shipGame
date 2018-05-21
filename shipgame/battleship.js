8d5751e4a8f7b942a1a7086c66cad6645e3e323f
unknown <zyq712911@outlook.com>
2018-05-21 10:46:36 +0800

    到书上384页

--- shipgame/battleship.js
+++ shipgame/battleship.js
@@ -12,12 +12,71 @@ var view = {
     cell.setAttribute("class","miss");
   }
 };
+var ship1 = {
+  location: ["10", "20", "30"],
+  hit: ["", "", ""]
+};
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships:[{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+         { locations: ["24", "34", "44"], hits: ["", "", ""] },
+         { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+
+  fire: function(guess){
+    for (var i = 0; i < this.numShips; i++){
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      if (index >= 0){
+          ship.hits[index] = "hit";
+          view.displayHit(guess);
+          view.displayMessage("HIT!");
+          if (this.isSunk(ship)){
+            view.displayMessage("You sank my battleship!");
+            this.shipsSunk++;
+          }
+          return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
+  },
+  isSunk: function(ship){
+    for(var i = 0; i< this.shipLength; i++){
+      if(ship.hits[i] !=="hit"){
+          return false;
+      }
+    }
+    return true;
+  }
+};
 view.displayMiss("00");
 view.displayHit("34");
-view.displayMiss("55");
 view.displayHit("12");
 view.displayMiss("25");
 view.displayHit("26");
+
+model.fire("53");
+
+model.fire("06");
+model.fire("16");
+model.fire("26");
+
+model.fire("34");
+model.fire("24");
+model.fire("44");
+
+model.fire("12");
+model.fire("11");
+model.fire("10");
+
+var ships = [{ locations: ["10", "20", "30"], hits: ["", "", ""] },
+             { locations: ["32", "33", "34"], hits: ["", "", ""] },
+             { locations: ["63", "64", "65"], hits: ["", "", "hit"] }];
 
 
 view.displayMessage("Tap tap, is this thing on?");
