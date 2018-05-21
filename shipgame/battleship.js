35960daf06654ba58ab2f0ff543ea4b860397696
unknown <zyq712911@outlook.com>
2018-05-21 11:41:34 +0800

    快做完成了

--- shipgame/battleship.js
+++ shipgame/battleship.js
@@ -22,9 +22,10 @@ var model = {
   shipLength: 3,
   shipsSunk: 0,
 
-  ships:[{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-         { locations: ["24", "34", "44"], hits: ["", "", ""] },
-         { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+
+  ships:[{ locations: [0,0, 0], hits: ["", "", ""] },
+         { locations: [0, 0, 0], hits: ["", "", ""] },
+         { locations: [0, 0, 0], hits: ["", "", ""] }],
 
   fire: function(guess){
     for (var i = 0; i < this.numShips; i++){
@@ -52,27 +53,67 @@ var model = {
       }
     }
     return true;
+  },
+  generateShipLocations: function(){},
+
+
+};
+function parseGuess(guess){
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+  if(guess === null || guess.length !== 2){
+    alert("Oops, please enter a letter and a number in the board.");
+  }else{
+    firtstChar = guess.charAt(0);
+    var row = alphabet.indexOf(firtstChar);
+    var column = guess.charAt(1);
+
+    if (isNaN(row) || isNaN(column)) {
+      alert("Oops, that isn't on the board.");
+    }else if (row < 0 || row >= model.boardSize ||
+               column < 0 || column >= model.boardSize){
+      alert("Oops, that's off the board!");
+    }else{
+      return row + column;
+    }
+  }
+  return null;
+}
+
+var controller = {
+  guesses: 0,
+
+  processGuess: function(guess){
+    var location = parseGuess(guess);
+    if (location){
+      this.guesses++;
+      var hit = model.fire(location);
+      if (hit && model.shipsSunk === model.numShips){
+            view.displayMessage("You sank all my battleships, in " + this.guesses + "gueses" );
+      }
+    }
   }
 };
-view.displayMiss("00");
-view.displayHit("34");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
+function init(){
+  var fireButton = document.getElementById("fireButton");
+  fireButton.onclick = handleFireButton;
+  var guessInput = document.getElementById("guessaInput");
+  guessInput.onkeypress = handleKeyPress;
+}
+function handleKeyPress(e){
+  var fireButton = document.getElementById("fireButton");
+  if (e.keyCode === 13){
+    fireButton.click();
+    return false;
+  }
+}
+function handleFireButton(){
+  var guessInput = document.getElementById("guessInput");
+  var guess = guessInput.value;
+  controller.processGuess(guess);
 
-model.fire("53");
-
-model.fire("06");
-model.fire("16");
-model.fire("26");
-
-model.fire("34");
-model.fire("24");
-model.fire("44");
-
-model.fire("12");
-model.fire("11");
-model.fire("10");
+  guessInput.value = "";
+}
+window.onload = init;
 
 var ships = [{ locations: ["10", "20", "30"], hits: ["", "", ""] },
              { locations: ["32", "33", "34"], hits: ["", "", ""] },
